b3251b96522c3c0660b2d60130b21d04de4e562d
Mherzog <matthewherzog4@gmail.com>
2022-12-05 14:10:06 -0500

    home page

--- pages/index.js
+++ pages/index.js
@@ -4,70 +4,21 @@ import Features from './features'
 import Script from 'next/script'
 import Navbar from './Navbar'
 import IframeResizer from 'iframe-resizer-react'
-
+import Contact from './contact'
+import Section from './section'
+import CTA from './CTA'
+import Footer from './footer'
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
 
-      <Navbar />
-      
-      <Head>
-        <title>Directystems.io</title>
-        <meta name="description" content="Directsystems.io" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <Features >
-    
-      </Features>
-
-      <IframeResizer src="https://embed.testimonial.to/w/mattherzog-testimonials?theme=light&card=base&showSearch=on&randomize=on&hideDate=on"  style={{ width: "1px", minWidth: "100%" }} />
+<Contact />
+<Features />
+<Section />
+<IframeResizer src="https://embed.testimonial.to/w/mattherzog-testimonials?theme=light&card=base&showSearch=on&randomize=on&hideDate=on"  style={{ width: "1px", minWidth: "100%"}} />
+<CTA />
 
 
       <footer className={styles.footer}>
@@ -83,7 +34,6 @@ export default function Home() {
       </footer>
     </div>
   )
-
 
 }
 
