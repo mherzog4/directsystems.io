1dcb88a1cf5048f96a7f956b19649d24fd6b102a
Mherzog <matthewherzog4@gmail.com>
2022-12-05 22:55:15 -0500

    jest test setup

---
+++ __tests__/contact.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Contact from '../pages/contact
+import '@testing-library/jest-dom'
+
+describe.todo('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next\.js!/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
